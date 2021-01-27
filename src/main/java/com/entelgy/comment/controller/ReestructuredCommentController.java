8e883f44c1363feb70296330d914123525a2b97e
Carlos Chavez <adagny>
2021-01-26 19:21:02 -0500

    formateo de clases

--- src/main/java/com/entelgy/comment/controller/ReestructuredCommentController.java
+++ src/main/java/com/entelgy/comment/controller/ReestructuredCommentController.java
@@ -1,7 +1,7 @@
 package com.entelgy.comment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ public class ReestructuredCommentController {
 	@Autowired
 	ReestructuredCommentService reestructuredCommentService;
 
-	@GetMapping("getAll")
+	@PostMapping("reestructureAll")
 	public ReestructuredComents getReestructuredComment() {
 		return reestructuredCommentService.getReestructuredComments();
 	}
--- src/main/java/com/entelgy/comment/service/ReestructuredCommentService.java
+++ src/main/java/com/entelgy/comment/service/ReestructuredCommentService.java
@@ -1,10 +1,8 @@
 package com.entelgy.comment.service;
 
-import java.util.List;
-
 import com.entelgy.comment.model.ReestructuredComents;
 
 public interface ReestructuredCommentService {
-	
+
 	ReestructuredComents getReestructuredComments();
 }
